e489cc8323d505995bb011f44e6fde2a0456c155
masacr3 ( leo R ) <leorhcp2030@gmail.com>
2021-01-09 09:40:14 -0300

    agrego coneccion a mongodb

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const productos = require('./routes/productos')
+const moongose = require('mongoose')
+
+moongose.connect('mongodb+srv://leopiola:martogato@cluster0.3jn0a.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser : true
+})
+.then( response =>{
+    console.log('Se conecto sastifactoriamente al servidor de la NSA')
+})
+.catch( error =>{
+    console.log('Error credenciales para conectarse a NSA')
+})
+
+
 
 app.use(express.json())
 app.use('/productos', productos)
